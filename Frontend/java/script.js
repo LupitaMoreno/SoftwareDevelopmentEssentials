7bd25cbbbf2b624f4bed542af94f4df8e7ae0657
Lupi <guadalupemq@gmail.com>
2024-10-26 10:50:37 -0600

    Actualizando toda la rama master con java, html

---
+++ Frontend/java/script.js
@@ -0,0 +1,23 @@
+//  Comentarios de una línea
+/* Bloque de comentario */
+let variable;
+console.log(typeof variable);
+
+let nombre = "Nombre";
+console.log(typeof nombre);
+
+let edad = 32;
+console.log(typeof edad);
+
+let trabaja = true;
+console.log(typeof trabaja);
+
+const mascota = "perro";
+console.log(mascota);
+
+mascota = "gato";
+
+console.log(mascota);
+
+let peso;
+peso = prompt()
---
+++ Frontend/java/sesion1/practica1/DatosPersonales.js
@@ -0,0 +1,28 @@
+
+let nombre;
+let edad;
+let direccion ;
+
+nombre = prompt("¿Cuál es tu nombre?");
+
+
+edad = prompt("¿Cuál es tu edad?");
+
+
+direccion = prompt("¿Cuál es la dirección?");
+
+let telefono;
+telefono = prompt("¿Cuál es tu teléfono?");
+
+let PuestoDeTrabajo;
+PuestoDeTrabajo = prompt("¿Cuál es tu puesto de trabajo?");
+
+let salario;
+salario = prompt("¿Cual es el salario");
+
+console.log(nombre);
+console.log(edad);
+console.log(direccion);
+console.log(telefono);
+console.log(PuestoDeTrabajo);
+console.log(salario);
